40686805085f32c76c2f271a06986078274f7843
Utkrist <utkrist.shrestha@creativevirtual.com>
2024-11-30 22:20:16 +1100

    tt

--- src/App.js
+++ src/App.js
@@ -8,18 +8,37 @@ import Projects from "./component/myProjects/projects";
 import Footer from "./component/footer/footer";
 import useLocalStorage from "use-local-storage";
 import Clouds from "./component/cloudsBG/clouds";
+import { useEffect } from 'react';
 
 function App() {
     const [theme, setTheme] = useLocalStorage("theme", "light");
+    const [timestamp, setTimestamp] = useLocalStorage("timestamp", null);
 
     const switchTheme = () => {
         const newTheme = theme === "light" ? "dark" : "light";
         const sun = document.querySelector(".sun");
         const moon = document.querySelector(".moon");
-        sun.classList.toggle('visible')
-        moon.classList.toggle('visible')
+        sun.classList.toggle("visible");
+        moon.classList.toggle("visible");
         setTheme(newTheme);
     };
+
+    useEffect(() => {
+        const currentTime = new Date().getTime();
+
+        if (!timestamp) {
+            setTimestamp(currentTime);
+        } else {
+            const timeDifference = currentTime - timestamp;
+
+            // if (timeDifference > 24 * 60 * 60 * 1000) {
+            if (timeDifference > 2 * 60 * 1000) {
+                localStorage.clear();
+                localStorage.setItem('need_nudge', 'true');
+                setTimestamp(currentTime);
+            }
+        }
+    }, [timestamp, setTimestamp]);
 
     return (
         <div className="App" data-theme={theme}>
--- src/component/hover/hover.js
+++ src/component/hover/hover.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./hover.css";
 import { useState, useRef } from "react";
 import { Link } from "react-scroll";
@@ -9,27 +9,6 @@ function Hover({ switchTheme, theme }) {
     const navIconsRef = useRef();
     const modeRef = useRef();
 
-    // const [theme, setTheme] = useLocalStorage("theme" ? "light" : "dark");
-    // const switchTheme = () => {
-    //   const newTheme = theme === "light" ? "dark" : "light";
-    //   setTheme(newTheme);
-    // };
-
-    // document.querySelector(".App").dataset.theme = theme;
-    // document.querySelector(".APP").setAttribute("data-theme", theme);
-
-    // const checkSize = () => {
-    //   if (window.innerWidth > 759) {
-    //     containers.forEach((element) => {
-    //       if (element.classList.contains("blur")) {
-    //         element.classList.remove("blur");
-    //       }
-    //     });
-    //   }
-    // };
-
-    // window.removeEventListener("resize", checkSize);
-
     const blurContainer = () => {
         const containers = document.querySelectorAll(".container");
         containers.forEach((element) => {
@@ -37,23 +16,75 @@ function Hover({ switchTheme, theme }) {
         });
     };
 
-    const onClickMenu = () => {
+    const onClickMenu = (target) => {
         if (window.innerWidth < 759) {
             blurContainer();
             setOpen(!open);
             navIconsRef.current.classList.toggle("openBar");
             modeRef.current.classList.toggle("openBar");
         }
+
+        const need_nudge = localStorage.getItem("need_nudge");
+        need_nudge && oncloseNudge(target);
     };
+
+    const oncloseNudge = (target) => {
+        console.log("Close clicked", target);
+        const nudge = document.querySelectorAll(".nudge");
+        const nudge_nav = document.querySelector(".nudgenav");
+        const nudge_theme = document.querySelector(".nudgetheme");
+        nudge.forEach((nudge) => {
+            nudge.style.display = "none";
+        });
+        if (target === "menu") {
+            nudge_theme.style.display = "block";
+        } else if (target === "theme") {
+            nudge_nav.style.display = "block";
+        } else {
+            localStorage.setItem("need_nudge", "false");
+        }
+    };
+
+    const checkNeedNudge = () => {
+        const need_nudge = localStorage.getItem("need_nudge");
+
+        if (need_nudge) {
+            const nudge_theme = document.querySelector(".nudgetheme");
+            const nudge_menu = document.querySelector(".nudgemenu");
+            if (nudge_theme) {
+                nudge_theme.style.display = "block";
+            }
+            if (nudge_menu) {
+                nudge_menu.style.display = "block";
+            }
+        }
+    };
+
+    useEffect(() => {
+        checkNeedNudge();
+    }, []);
+
     return (
         <>
             <div id="nav" className="nav">
-                <div className="dropDownMenu" onClick={() => onClickMenu()}>
+                <div
+                    className="dropDownMenu"
+                    onClick={() => onClickMenu("menu")}
+                >
                     {open ? (
                         <i className="fa-solid fa-xmark"></i>
                     ) : (
                         <i className="fa-solid fa-ellipsis"></i>
                     )}
+                </div>
+                <div className="nudge nudgemenu">
+                    <div
+                        className="nudge-close"
+                        onClick={() => oncloseNudge("menu")}
+                    >
+                        <i className="fa-solid fa-xmark" aria-hidden="true"></i>
+                    </div>
+                    <p>Click the here to check the menu</p>
                 </div>
                 <ul ref={navIconsRef} className="navIcons">
                     {/* <a href="#home"> */}
@@ -66,7 +97,7 @@ function Hover({ switchTheme, theme }) {
                     >
                         <li
                             href="home"
-                            onClick={() => onClickMenu()}
+                            onClick={() => onClickMenu("nav")}
                             className="icon homeBtn"
                         >
                             <span className="iconDetail">Home</span>
@@ -85,7 +116,7 @@ function Hover({ switchTheme, theme }) {
                         duration={500}
                     >
                         <li
-                            onClick={() => onClickMenu()}
+                            onClick={() => onClickMenu("nav")}
                             className="icon aboutBtn"
                         >
                             <span className="iconDetail">About</span>
@@ -103,7 +134,7 @@ function Hover({ switchTheme, theme }) {
                         duration={500}
                     >
                         <li
-                            onClick={() => onClickMenu()}
+                            onClick={() => onClickMenu("nav")}
                             className="icon educationBtn"
                         >
                             <span className="iconDetail">Education</span>
@@ -120,7 +151,7 @@ function Hover({ switchTheme, theme }) {
                         duration={500}
                     >
                         <li
-                            onClick={() => onClickMenu()}
+                            onClick={() => onClickMenu("nav")}
                             className="icon skillsBtn"
                         >
                             <span className="iconDetail">Skills</span>
@@ -137,7 +168,7 @@ function Hover({ switchTheme, theme }) {
                         duration={500}
                     >
                         <li
-                            onClick={() => onClickMenu()}
+                            onClick={() => onClickMenu("nav")}
                             className="icon projectsBtn"
                         >
                             <span className="iconDetail">Projects</span>
@@ -146,32 +177,51 @@ function Hover({ switchTheme, theme }) {
                             </span>
                         </li>
                     </Link>
+                    <div className="nudge nudgenav">
+                        <div
+                            className="nudge-close"
+                            onClick={() => oncloseNudge("nav")}
+                        >
+                            <i
+                                className="fa-solid fa-xmark"
+                                aria-hidden="true"
+                            ></i>
+                        </div>
+                        <p>Navigate in the website form here</p>
+                    </div>
                 </ul>
             </div>
-            <div
-                ref={modeRef}
-                onClick={() => onClickMenu()}
-                className="mode"
-                id="mode"
-            >
+            <div ref={modeRef} className="mode" id="mode">
                 <button
-                    class="theme"
+                    className="theme"
                     aria-label="Toggle color mode"
                     title="Toggle color mode"
-                    onClick={() => switchTheme()}
+                    onClick={() => {
+                        onClickMenu("theme");
+                        switchTheme();
+                    }}
                 >
-                    <div class="sun visible"></div>
-                    <div class="moon">
-                        <div class="star"></div>
-                        <div class="star small"></div>
+                    <div className="sun visible"></div>
+                    <div className="moon">
+                        <div className="star"></div>
+                        <div className="star small"></div>
                     </div>
                 </button>
                 {/* <span onClick={() => switchTheme()}>
           <i className="fa-regular fa-lightbulb"></i>
         </span> */}
                 <span className="modeDetail">
-          {theme === "light" ? "Go Night" : "Go Day"}
-        </span>
+                    {theme === "light" ? "Go Night" : "Go Day"}
+                </span>
+                <div className="nudge nudgetheme">
+                    <div
+                        className="nudge-close"
+                        onClick={() => oncloseNudge("theme")}
+                    >
+                        <i className="fa-solid fa-xmark" aria-hidden="true"></i>
+                    </div>
+                    <p>Click the here to change the theme</p>
+                </div>
             </div>
         </>
     );
